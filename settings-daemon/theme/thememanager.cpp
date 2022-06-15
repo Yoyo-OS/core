9eb67622e2b577e02eedb6b16612eef3c3e66c05
柚子 <40852301+uiYzzi@users.noreply.github.com>
2022-06-16 03:16:29 +0800

    update background visible settings

--- settings-daemon/theme/thememanager.cpp
+++ settings-daemon/theme/thememanager.cpp
@@ -68,7 +68,7 @@ ThemeManager::ThemeManager(QObject *parent)
     // init value
     m_isDarkMode = m_settings->value("DarkMode", false).toBool();
     m_darkModeDimsWallpaer = m_settings->value("DarkModeDimsWallpaer", false).toBool();
-    m_backgroundVisible = m_settings->value("BackgroundVisible", true).toBool();
+    m_backgroundVisible = true;
     m_wallpaperPath = m_settings->value("Wallpaper", "/usr/share/backgrounds/yoyoos/default.jpg").toString();
     m_accentColor = m_settings->value("AccentColor", 0).toInt();
     m_backgroundType = m_settings->value("BackgroundType", 0).toInt();
@@ -140,7 +140,6 @@ void ThemeManager::setDarkModeDimsWallpaer(bool value)
         return;
 
     m_darkModeDimsWallpaer = value;
-    m_settings->setValue("DarkModeDimsWallpaer", m_darkModeDimsWallpaer);
 
     emit darkModeDimsWallpaerChanged();
 }
