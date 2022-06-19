e11c210469ef17d0b63258ba76abcbe1c302a493
柚子 <40852301+uiYzzi@users.noreply.github.com>
2022-06-19 17:40:12 +0800

    0.9

--- session/processmanager.cpp
+++ session/processmanager.cpp
@@ -163,6 +163,8 @@ void ProcessManager::startDaemonProcess()
     list << qMakePair(QString("yoyo-digitalwellbeing-daemon"), QStringList());
     list << qMakePair(QString("yoyo-xembedsniproxy"), QStringList());
     list << qMakePair(QString("yoyo-gmenuproxy"), QStringList());
+    list << qMakePair(QString("yoyo-getfastmirror"),QStringList());
+    list << qMakePair(QString("yoyo-permission-surveillance"),QStringList());
     //list << qMakePair(QString("chotkeys"), QStringList());
 
     for (QPair<QString, QStringList> pair : list) {
